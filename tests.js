f3823955ef1689f03b3361c2522a30857f9d6d9c
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-05-24 18:22:36 -0600

    more stubbing, more TDD

--- tests.js
+++ tests.js
@@ -4,114 +4,104 @@ process.env['PORT'] = '0'; // for testing bind ephemeral ports
 const
 should = require('should'),
 http = require('http'),
-net = require('net'),
-webserver = require('./bin/keysigner');
+server = require('./bin/keysigner'),
+jwcrypto = require('jwcrypto');
 
-var webPort = -1;
+// cause RSA and DSA algorithms to be loaded
+// (Dear @benadida.  This API sucks.  Thanks, @lloyd)
+require("jwcrypto/lib/algs/rs");
+require("jwcrypto/lib/algs/ds");
+
+// a little helper function to perform cert_key requests
+function doRequest(args, cb) {
+  var body = JSON.stringify(args);
+
+  var req = http.request({
+    host: '127.0.0.1',
+    port: serverPort,
+    path: '/cert_key',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': body.length
+    }
+  }, cb);
+
+  req.write(body);
+  req.end();
+}
+
+var serverPort = -1;
 
 describe('the server', function() {
   it('should start up', function(done) {
-    webserver(function(err, port) {
+    server(function(err, port) {
       should.not.exist(err);
       (port).should.be.ok
-      webPort = port;
+      serverPort = port;
       done();
     });
   });
 });
 
-/*
+var keyPair;
 
-describe('clearing email', function() {
+describe('key generation', function() {
   it('should work', function(done) {
-    http.request({
-      host: '127.0.0.1',
-      port: webPort,
-      path: '/mail/me',
-      method: 'DELETE'
-    }, function(res) {
-      (res.statusCode).should.equal(200);
-      done();
-    }).end();
-  });
-});
-
-describe('sending email', function() {
-  it('should work', function(done) {
-    var s = net.connect(emailPort, function(err) {
+    jwcrypto.generateKeypair({
+      algorithm: 'DS',
+      keysize: 256
+    }, function(err, kp) {
       should.not.exist(err);
-
-      var response = "";
-      s.on('data', function(chunk) { response += chunk; });
-
-      s.on('end', function() {
-        response.split('\r\n')[6].should.equal('221 Bye!');
-        s.destroy();
-        done();
-      });
-
-      s.end("helo\nmail from: <lloyd@localhost>\nrcpt to: <me@localhost>\ndata\nfrom: lloyd <lloyd@localhost>\nto: me <me@localhost>\n\nhi\n.\nquit\n");
-    });
-    setTimeout(function() { console.log('waited'); });
-  });
-});
-
-describe('web apis', function() {
-  it('should return mail', function(done) {
-    http.request({
-      host: '127.0.0.1',
-      port: webPort,
-      path: '/mail/me',
-      method: 'GET'
-    }, function(res) {
-      (res.statusCode).should.equal(200);
-      var data = "";
-      res.on('data', function (chunk) { data += chunk; });
-      res.on('end', function () {
-        data = JSON.parse(data);
-        data.length.should.equal(1);
-        data = data[0];
-        data.text.should.equal('hi\n');
-        data.from[0].address.should.equal('lloyd@localhost');
-        data.from[0].name.should.equal('lloyd');
-        data.to[0].address.should.equal('me@localhost');
-        data.to[0].name.should.equal('me');
-        Object.keys(data.headers).length.should.equal(2);
-        done();
-      });
-    }).end();
-  });
-});
-
-describe('clearing email', function() {
-  it('should work', function(done) {
-    http.request({
-      host: '127.0.0.1',
-      port: webPort,
-      path: '/mail/me',
-      method: 'DELETE'
-    }, function(res) {
-      (res.statusCode).should.equal(200);
+      kp.should.be.a('object');
+      keyPair = kp;
       done();
-    }).end();
-  });
-
-  it('should cause GET to return zero mails', function(done) {
-    http.request({
-      host: '127.0.0.1',
-      port: webPort,
-      path: '/mail/me',
-      method: 'GET'
-    }, function(res) {
-      (res.statusCode).should.equal(200);
-      var data = "";
-      res.on('data', function (chunk) { data += chunk; });
-      res.on('end', function () {
-        data = JSON.parse(data);
-        data.length.should.equal(0);
-        done();
-      });
-    }).end();
+    });
   });
 });
-*/
+
+describe('key certification', function() {
+  it('should work', function(done) {
+    doRequest({
+      duration: (6 * 60 * 60 * 1000), // 6 hours
+      pubkey: keyPair.publicKey.serialize(),
+      email: 'lloyd@example.com'
+    }, function(res) {
+      (res.statusCode).should.equal(200);
+      // XXX: more thorough testing of response required
+      done();
+    });
+  });
+
+  it('should fail when duration is not a number', function(done) {
+    doRequest({
+      duration: (6 * 60 * 60 * 1000).toString(), // 6 hours
+      pubkey: keyPair.publicKey.serialize(),
+      email: 'lloyd@example.com'
+    }, function(res) {
+      (res.statusCode).should.equal(400);
+      done();
+    });
+  });
+
+  it('should fail when email is not provided', function(done) {
+    doRequest({
+      duration: (6 * 60 * 60 * 1000).toString(), // 6 hours
+      pubkey: keyPair.publicKey.serialize()
+    }, function(res) {
+      (res.statusCode).should.equal(400);
+      done();
+    });
+  });
+
+  it('should fail when email is not a string', function(done) {
+    doRequest({
+      duration: (6 * 60 * 60 * 1000).toString(), // 6 hours
+      pubkey: keyPair.publicKey.serialize(),
+      email: 42
+    }, function(res) {
+      (res.statusCode).should.equal(400);
+      done();
+    });
+  });
+});
